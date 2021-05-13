06af54590bfa718556cf8e6873c0b71f6cdae7cf
ankeet7x <ankeetkarki30@gmail.com>
2021-05-13 07:32:28 +0545

    Init

---
+++ lib/pages/signup.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SignupView extends StatefulWidget {
+  @override
+  _SignupViewState createState() => _SignupViewState();
+}
+
+class _SignupViewState extends State<SignupView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

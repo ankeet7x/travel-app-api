e3d29331713b89bd46fdcc9d4810c1a159a732d7
ankeet7x <ankeetkarki30@gmail.com>
2021-05-13 08:12:05 +0545

    mongoose setup

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+
+mongoose.connect('mongodb+srv://ankeet:'+ process.env.MONGO_ATLAS_PW +'@cluster0.aaofu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true});
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+
+app.get('/', (req, res) => {
+    res.send('api is live');
+});
+
+if (process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status(404);
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message
+        }
+    });
+});
+
+module.exports = app;
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'views/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Travel App",
-      home: Home(),
+      home: HomeView(),
     );
   }
 }
